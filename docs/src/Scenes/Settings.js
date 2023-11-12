66a5b93a0b86299ca6c201a698f91f478e3b9447
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2023-11-12 14:35:16 +0100

    Update Settings.js
    Buscar assets botones, mute hecho y recuperado lo que se borró de mi settings.js con el fetch

--- docs/src/Scenes/Settings.js
+++ docs/src/Scenes/Settings.js
@@ -8,77 +8,117 @@ export class Settings extends Phaser.Scene {
     preload() {
         this.load.audio('menuMusic', './assets/audio/MenuMusic.mp3');
         this.load.image('background', './assets/images/background/MenuBackground.png');
+        // Asegúrate de tener imágenes para los botones de subir/bajar volumen y brillo
+        this.load.image('buttonUp', './assets/images/buttons/buttonUp.png');
+        this.load.image('buttonDown', './assets/images/buttons/buttonDown.png');
+        this.load.image('muteButton', './assets/images/buttons/muteButton.png'); // Botón de silencio
     }
 
     create() {
-        // Música de fondo
-        this.backgroundMusic = this.sound.add('menuMusic', { volume: gameSettings.musicVolume });
+        this.createBackground();
+        this.createMusic();
+        this.createTitle();
+        this.createVolumeControls();
+        this.createBrightnessControls();
+        this.createBackButton();
+        this.createFadeIn();
+    }
+
+    createBackground() {
+        this.bg = this.add.image(0, 0, 'background')
+            .setOrigin(0, 0)
+            .setDisplaySize(this.game.config.width, this.game.config.height)
+            .setAlpha(gameSettings.brightness);
+    }
+
+    createMusic() {
+        this.backgroundMusic = this.sound.add('menuMusic', { volume: gameSettings.musicVolume, loop: true });
         if (!this.backgroundMusic.isPlaying) {
-            this.backgroundMusic.play({ loop: true });
+            this.backgroundMusic.play();
         }
+    }
 
-        // Imagen de fondo con transparencia según el brillo establecido
-        this.bg = this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(this.game.config.width, this.game.config.height).setAlpha(gameSettings.brightness);
+    createTitle() {
+        let centerY = this.game.config.height * 0.5;
+        this.add.text(this.game.config.width * 0.5, centerY - 200, 'CONFIGURACIONES', { font: '64px Arial', fill: '#ffffff' }).setOrigin(0.5);
+    }
 
+    createVolumeControls() {
         let centerY = this.game.config.height * 0.5;
 
-        // Título
-        this.add.text(this.game.config.width * 0.5, centerY - 150, 'CONFIGURACIONES', { font: '64px Arial', fill: '#000000' }).setOrigin(0.5);
+        // Texto del volumen
+        this.add.text(this.game.config.width * 0.3, centerY - 50, 'Volumen:', { font: '32px Arial', fill: '#ffffff' }).setOrigin(0.5);
+        this.volumeText = this.add.text(this.game.config.width * 0.7, centerY - 50, `${Math.round(this.sound.volume * 100)}%`, { font: '32px Arial', fill: '#ffffff' }).setOrigin(0.5);
 
-        // Opciones de volumen
-        this.add.text(this.game.config.width * 0.3, centerY - 50, 'Volumen:', { font: '48px Arial', fill: '#000000' }).setOrigin(0.5);
-        this.volumeText = this.add.text(this.game.config.width * 0.7, centerY - 50, `${Math.round(this.sound.volume * 100)}%`, { font: '48px Arial', fill: '#000000' }).setOrigin(0.5).setInteractive();
+        // Botones de subir/bajar volumen
+        let volumeUpButton = this.add.image(this.game.config.width * 0.8, centerY - 50, 'buttonUp').setInteractive();
+        let volumeDownButton = this.add.image(this.game.config.width * 0.6, centerY - 50, 'buttonDown').setInteractive();
 
-        this.volumeText.on('pointerup', () => {
-            let currentVolume = this.sound.volume;
-            if (currentVolume <= 0.1) {
-                currentVolume = 1;
-            } else {
-                currentVolume -= 0.1;
-            }
-            this.sound.setVolume(currentVolume);
-            this.volumeText.setText(`${Math.round(currentVolume * 100)}%`);
-            gameSettings.musicVolume = currentVolume; // Almacenar el volumen actualizado
+        volumeUpButton.on('pointerup', () => {
+            this.adjustVolume(0.1);
         });
 
-        // Opciones de brillo
-        this.add.text(this.game.config.width * 0.3, centerY, 'Brillo:', { font: '48px Arial', fill: '#000000' }).setOrigin(0.5);
-        this.brightnessText = this.add.text(this.game.config.width * 0.7, centerY, `${Math.round(gameSettings.brightness * 100)}%`, { font: '48px Arial', fill: '#000000' }).setOrigin(0.5).setInteractive();
-
-        this.brightnessText.on('pointerup', () => {
-            let currentBrightness = this.bg.alpha;
-            if (currentBrightness <= 0.1) {
-                currentBrightness = 1;
-            } else {
-                currentBrightness -= 0.1;
-            }
-            this.bg.setAlpha(currentBrightness);
-            this.brightnessText.setText(`${Math.round(currentBrightness * 100)}%`);
-            gameSettings.brightness = currentBrightness; // Almacenar el brillo actualizado
+        volumeDownButton.on('pointerup', () => {
+            this.adjustVolume(-0.1);
         });
 
-        // Botón de volver
-        let backButton = this.add.text(this.game.config.width * 0.5, centerY + 100, 'Volver', { font: '48px Arial', fill: '#000000' }).setOrigin(0.5).setInteractive();
-
-        // Animación inicial (fade-in)
-        this.cameras.main.setAlpha(0);
-        this.tweens.add({
-            targets: this.cameras.main,
-            alpha: 1,
-            duration: 1500
-        });
-
-        backButton.on('pointerup', () => {
-            this.backgroundMusic.stop(); // Detener la música al salir de la escena de configuración
-            this.tweens.add({
-                targets: this.cameras.main,
-                alpha: 0,
-                duration: 1500,
-                onComplete: () => {
-                    this.scene.start('Menu');
-                }
-            });
+        // Botón de silencio
+        let muteButton = this.add.image(this.game.config.width * 0.9, centerY - 50, 'muteButton').setInteractive();
+        muteButton.on('pointerup', () => {
+            this.toggleMute();
         });
     }
-}
 
+    createBrightnessControls() {
+        let centerY = this.game.config.height * 0.5;
+
+        // Texto del brillo
+        this.add.text(this.game.config.width * 0.3, centerY, 'Brillo:', { font: '32px Arial', fill: '#ffffff' }).setOrigin(0.5);
+        this.brightnessText = this.add.text(this.game.config.width * 0.7, centerY, `${Math.round(gameSettings.brightness * 100)}%`, { font: '32px Arial', fill: '#ffffff' }).setOrigin(0.5);
+
+        // Botones de subir/bajar brillo
+        let brightnessUpButton = this.add.image(this.game.config.width * 0.8, centerY, 'buttonUp').setInteractive();
+        let brightnessDownButton = this.add.image(this.game.config.width * 0.6, centerY, 'buttonDown').setInteractive();
+
+        brightnessUpButton.on('pointerup', () => {
+            this.adjustBrightness(0.1);
+        });
+
+        brightnessDownButton.on('pointerup', () => {
+            this.adjustBrightness(-0.1);
+        });
+    }
+
+    createBackButton() {
+        let centerY = this.game.config.height * 0.5;
+        let backButton = this.add.text(this.game.config.width * 0.5, centerY + 150, 'Volver', { font: '32px Arial', fill: '#ffffff' }).setOrigin(0.5).setInteractive();
+
+        backButton.on('pointerup', () => {
+            this.backgroundMusic.stop();
+            this.scene.start('Menu');
+        });
+    }
+
+    createFadeIn() {
+        this.cameras.main.fadeIn(1500, 0, 0, 0);
+    }
+
+    adjustVolume(change) {
+        let newVolume = Phaser.Math.Clamp(this.sound.volume + change, 0, 1);
+        this.sound.setVolume(newVolume);
+        this.volumeText.setText(`${Math.round(newVolume * 100)}%`);
+        gameSettings.musicVolume = newVolume;
+    }
+
+    adjustBrightness(change) {
+        let newBrightness = Phaser.Math.Clamp(gameSettings.brightness + change, 0, 1);
+        this.bg.setAlpha(newBrightness);
+        this.brightnessText.setText(`${Math.round(newBrightness * 100)}%`);
+        gameSettings.brightness = newBrightness;
+    }
+
+    toggleMute() {
+        this.backgroundMusic.mute = !this.backgroundMusic.mute;
+        this.volumeText.setText(this.backgroundMusic.mute ? 'Mute' : `${Math.round(this.sound.volume * 100)}%`);
+    }
+}

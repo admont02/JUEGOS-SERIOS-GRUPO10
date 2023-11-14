6b1154ea3a07dc6ba64f55f81d8d1998d186e324
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2023-11-14 14:54:23 +0100

    Calle dialogo clic

--- docs/src/Scenes/GameScene.js
+++ docs/src/Scenes/GameScene.js
@@ -18,7 +18,19 @@ export class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('willy', 'assets/images/characters/balta.jpg');   
     }
-    
+
+    showOptions() {
+        this.dialogModal.toggleWindow();
+        
+        const repeatButton = this.add.text(100, 100, 'Repetir Conversación', { fill: '#0f0' })
+            .setInteractive()
+            .on('pointerdown', () => this.reopenDialog());
+
+        const closeButton = this.add.text(100, 150, 'Cerrar Diálogo', { fill: '#f00' })
+            .setInteractive()
+            .on('pointerdown', () => this.closeDialog());
+    }
+
     create() {
         this.willy = new Willy(this, this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'willy');
         this.dialogModal = new DialogModal(this);
@@ -26,30 +38,18 @@ export class GameScene extends Phaser.Scene {
         this.dialogModal.doubleFontSize();
         
         this.printDialog();
-        this.dialogTimer = this.time.addEvent({
-            delay: 3000,
-            callback: this.printDialog,
-            callbackScope: this,
-            loop: true
-        });
+        this.input.on('pointerdown', this.changeDialog, this);
     }
-    
-    printDialog() {
+
+    changeDialog() {
         if (this.dialogIndex < this.dialogs.length) {
             this.dialogModal.setText(this.dialogs[this.dialogIndex], true);
             this.dialogPrinted = true;
             this.dialogIndex++;
         } else {
-            this.dialogModal.toggleWindow();
-            this.dialogTimer.remove(false);
-            this.createReopenDialogButton();
+            this.showOptions();
+            this.input.off('pointerdown', this.changeDialog, this);
         }
-    }
-
-    createReopenDialogButton() {
-        const button = this.add.text(100, 100, 'Reopen Dialog', { fill: '#0f0' })
-            .setInteractive()
-            .on('pointerdown', () => this.reopenDialog());
     }
 
     reopenDialog() {
@@ -57,17 +57,28 @@ export class GameScene extends Phaser.Scene {
         this.dialogPrinted = false;
         this.dialogModal.toggleWindow();
         this.printDialog();
-        this.dialogTimer = this.time.addEvent({
-            delay: 3000,
-            callback: this.printDialog,
-            callbackScope: this,
-            loop: true
-        });
+        this.input.on('pointerdown', this.changeDialog, this);
+    }
+
+    closeDialog() {
+        this.dialogModal.toggleWindow();
+        // Puedes agregar acciones adicionales al cerrar el diálogo si es necesario.
     }
 
     update(time, delta) {
         if (this.willy.update) {
             this.willy.update(time, delta);
+        }
+    }
+
+    printDialog() {
+        if (this.dialogIndex < this.dialogs.length) {
+            this.dialogModal.setText(this.dialogs[this.dialogIndex], true);
+            this.dialogPrinted = true;
+            this.dialogIndex++;
+        } else {
+            this.showOptions();
+            this.input.off('pointerdown', this.changeDialog, this);
         }
     }
 }

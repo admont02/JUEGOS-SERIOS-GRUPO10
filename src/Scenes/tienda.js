1f82c9172c1fc3f751151b863260c76de0499156
admont02 <admont02@ucm.es>
2023-12-12 15:56:30 +0100

    caja se rompe al chocar con empleado

--- src/Scenes/tienda.js
+++ src/Scenes/tienda.js
@@ -1,5 +1,5 @@
 import { gameSettings } from './menu.js';
-import Willy from '../Characters/Willy.js'; 
+import Willy from '../Characters/Willy.js';
 import DialogModal from '../Text/plugText.js';
 import dialogos from '../dialogs.js';
 
@@ -8,7 +8,7 @@ export class ShopScene extends Phaser.Scene {
         super({ key: 'ShopScene' });
         this.dialogModal = null;
         this.currentDialogIndex = 0;
-        this.optionTexts = []; 
+        this.optionTexts = [];
         this.willy = null;
         this.dialogs = dialogos.shopDialogs;
         this.mujerDialogs = dialogos.shopWorkerDialogs; // Add this line
@@ -25,18 +25,18 @@ export class ShopScene extends Phaser.Scene {
         this.car = this.physics.add.sprite((0, 400), 0, 'car');
         this.car.y = 1350;
         this.car.setScale(0.5);
-        this.car.setVelocity(100,0);
+        this.car.setVelocity(100, 0);
         this.car.body.allowGravity = false;
         this.willy = new Willy(this, this.sys.game.config.width / 2, this.sys.game.config.height - 400, 'jugador');
         this.willy.body.setCollideWorldBounds(true);
         this.willy.body.setAllowGravity(false); // Asegúrate de que la gravedad esté configurada según tus necesidades
-    
+
         // Creación de la caja con física habilitada
-        const caja = this.physics.add.sprite(this.sys.game.config.width / 5, this.sys.game.config.height - 400, 'caja');
-        caja.body.setAllowGravity(false); // Ajusta la gravedad según sea necesario
-        caja.body.setImmovable(true);
+        this.caja = this.physics.add.sprite(this.sys.game.config.width / 5, this.sys.game.config.height - 400, 'caja');
+        this.caja.body.setAllowGravity(false); // Ajusta la gravedad según sea necesario
+        this.caja.body.setImmovable(true);
         // Establecer colisión entre Willy y la caja
-        this.physics.add.collider(this.willy, caja, this.colisionHandler);
+        this.physics.add.collider(this.willy, this.caja, this.colisionHandler);
 
         this.anims.create({
             key: 'walk',
@@ -48,7 +48,7 @@ export class ShopScene extends Phaser.Scene {
         // Coloca a la mujer fuera de la pantalla en el lado derecho y muévela hacia la izquierda
         this.shopWorker = this.physics.add.sprite(this.game.config.width - 100, this.game.config.height - 400, 'mujerCoche');
         this.shopWorker.body.allowGravity = false;
-      //  this.stupidwomen.setVelocity(-100,0); // Movimiento hacia la izquierda
+        //  this.stupidwomen.setVelocity(-100,0); // Movimiento hacia la izquierda
 
         // Flip horizontal si es necesario
         this.shopWorker.flipX = true;
@@ -59,20 +59,24 @@ export class ShopScene extends Phaser.Scene {
             this.dialogStarted = true;
             this.showMujerDialog();
         });
+        this.physics.add.collider(this.shopWorker, this.caja, this.collisionCallback, null, this);
 
-     
+
     }
-    
-colisionHandler(willy, caja) {
-    console.log("Colisión detectada entre Willy y la caja");
+    collisionCallback() {
+        // Destruir la caja cuando haya colisión
+        this.caja.destroy();
+    }
+    colisionHandler(willy, caja) {
+        console.log("Colisión detectada entre Willy y la caja");
 
-}
+    }
 
     showMujerDialog() {
-        this.removeOptions(); 
+        this.removeOptions();
         this.dialogModal.removeCharacterImage();
         this.dialogModal.createCharacterImage('caraMujer', 0.7);
-        
+
         if (this.currentDialogIndex >= this.mujerDialogs.length || this.currentDialogIndex === -1) {
             // Manejo del final del diálogo
             this.endDialogAndExitWoman();
@@ -95,80 +99,79 @@ colisionHandler(willy, caja) {
             if (option.box) option.box.destroy();
         });
         this.optionTexts = [];
-    
+
         options.forEach((option, index) => {
             let textHeight = 0;
             let textWidth = 0;
-    
+
             let dialogBox = this.add.graphics();
             dialogBox.fillStyle(0x000000, 0.5);  // Color y transparencia de la caja
-    
+
             let optionText = this.add.text(0, 0, option.text, { fill: '#fff', fontSize: '42px' });
             optionText.setResolution(1.1); // Ajuste para mejorar la calidad del texto
             textWidth = optionText.width + 40;  // Margen aumentado
             textHeight = optionText.height + 20; // Altura ajustada para el nuevo tamaño del texto
-    
+
             // Aumentar el espacio entre cada caja de opción
             const spacing = 100; // Ajusta este valor según sea necesario
             let boxY = 100 + (index * (textHeight + spacing));
-    
+
             // Dibujar la caja de diálogo con las dimensiones adecuadas
-            dialogBox.fillRect(100, boxY, textWidth, textHeight); 
-    
+            dialogBox.fillRect(100, boxY, textWidth, textHeight);
+
             // Posicionar el texto y hacerlo interactivo, colocándolo encima de la caja
             optionText.setPosition(110, boxY + 10);
             optionText.setInteractive()
                 .on('pointerup', () => this.handleOptionSelect(option.nextDialogIndex, index));
-    
+
             // Almacenar tanto la caja como el texto en optionTexts
             this.optionTexts.push({ box: dialogBox, text: optionText });
         });
     }
-    
-    
-    
+
+
+
     removeOptions() {
         this.optionTexts.forEach(option => {
             option.text.destroy();
             option.box.destroy();
         });
-        this.optionTexts = []; 
+        this.optionTexts = [];
     }
-    
-    
-    
+
+
+
     handleOptionSelect(nextDialogIndex, index) {
         console.log("Seleccionaste la opción:", index);
-    
+
         if (nextDialogIndex === -1) {
             this.endDialogAndExitWoman();
             return;
         }
 
-        if(nextDialogIndex === 7 || nextDialogIndex === 8 || nextDialogIndex === 9 || nextDialogIndex === 10){
+        if (nextDialogIndex === 7 || nextDialogIndex === 8 || nextDialogIndex === 9 || nextDialogIndex === 10) {
             gameSettings.lateBecauseOfWoman = true; // la mujer insulta al jugador
         }
-        
+
         this.currentDialogIndex = nextDialogIndex; // Actualiza el índice del diálogo
         this.showMujerDialog(); // Muestra el siguiente diálogo
     }
-    
-    
+
+
     endDialogAndExitWoman() {
         // Mueve a la mujer hacia la derecha para salir de la escena
-        this.shopWorker.setVelocity(100, 0);
-      
+        this.physics.moveToObject(this.shopWorker, this.caja, 70);
         // Espera un tiempo antes de destruir el sprite
         this.time.delayedCall(3000, () => {
             this.removeOptions();
             this.dialogModal.removeCharacterImage();
             this.dialogModal.toggleWindow();
-            this.shopWorker.destroy();
+            // this.shopWorker.destroy();
         });
-        this.willy.setMovable(true); 
+        this.willy.setMovable(true);
         this.canMove = true;
     }
-    
+
     removeCharacterImage() {
         if (this.characterImage) {
             this.characterImage.destroy();
@@ -194,24 +197,24 @@ colisionHandler(willy, caja) {
             this.dialogStarted = true; // Indica que el diálogo ha comenzado
         }
 
-       
-    
+
+
         // Lógica de movimiento de Willy
         if (this.willy && this.willy.update && this.canMove) {
             this.willy.update(time, delta);
-        
+
             let pointer = this.input.activePointer;
             if (pointer.isDown) {
                 this.willy.flipX = pointer.worldX < this.willy.x;
             }
-    
+
             // Restringe a Willy a no moverse más allá del borde izquierdo de la pantalla
             if (this.willy.x < 200) {
                 this.willy.x = 200;
             }
-    
+
             // Reproduce la animación de caminar si Willy se está moviendo
-            if (this.willy.velocity !== 0) { 
+            if (this.willy.velocity !== 0) {
                 this.willy.anims.play('walk', true);
             } else {
                 this.willy.anims.stop();

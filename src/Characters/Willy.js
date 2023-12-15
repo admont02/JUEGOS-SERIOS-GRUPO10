c07a0e2e71d3dce2bb3b7730b2572fd8d619c410
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2023-12-16 00:00:54 +0100

    FIX WILLY

--- src/Characters/Willy.js
+++ src/Characters/Willy.js
@@ -1,15 +1,15 @@
 export default class Willy extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, textureKey) {
         super(scene, x, y, textureKey);
-        
         this.scene.add.existing(this);
         this.setScale(2);
 
         // Asegurarse de que Willy tiene un cuerpo físico para usar setVelocity
         this.scene.physics.world.enable(this);
-      //  this.body.setCollideWorldBounds(true); // Para evitar que salga del mundo
-
+        // this.body.setCollideWorldBounds(true); // Para evitar que salga del mundo
+        this.body.allowGravity = false;
         this.canMove = false; // Flag para controlar el movimiento
+        this.destinationX = null; // Nueva variable para almacenar el destino
 
         this.scene.input.on('pointerdown', this.onPointerDown, this);
     }
@@ -20,20 +20,38 @@ export default class Willy extends Phaser.GameObjects.Sprite {
 
     onPointerDown(pointer) {
         if (!this.canMove) return; // Verificar si el movimiento está permitido
-
-        const speed = 200; // Velocidad de movimiento
-
-        // Calcular la dirección del movimiento
-        const direction = pointer.x >= this.x ? 1 : -1;
-        
-        // Establecer la velocidad de Willy
-        this.body.setVelocityX(speed * direction);
+        this.destinationX = pointer.x; // Establecer el destino donde el jugador hizo clic
     }
 
     update() {
-        // Si Willy no se está moviendo hacia un destino, detener su movimiento
         if (!this.canMove) {
             this.body.setVelocityX(0);
+            this.anims.stop();
+            return;
+        }
+        
+        // Añadir una tolerancia para determinar si Willy está cerca de su destino
+        const destinationTolerance = 4;
+    
+        if (this.destinationX !== null) {
+            const distanceToDestination = Math.abs(this.x - this.destinationX);
+            if (distanceToDestination <= destinationTolerance) {
+                // Willy ha alcanzado su destino
+                this.body.setVelocityX(0);
+                this.anims.stop();
+                this.destinationX = null;
+            } else {
+                // Mover a Willy hacia el destino
+                const speed = 200;
+                const direction = this.destinationX > this.x ? 1 : -1;
+                this.body.setVelocityX(speed * direction);
+                this.anims.play('walk', true);
+            }
+        } else {
+            // No hay destino, detener a Willy
+            this.body.setVelocityX(0);
+            this.anims.stop();
         }
     }
-}
+    
+}    
--- src/Scenes/menu.js
+++ src/Scenes/menu.js
@@ -56,8 +56,8 @@ export class Menu extends Phaser.Scene {
                 alpha: 0,
                 duration: 1500,
                 onComplete: () => {
-                    this.scene.start('ShopScene');
-                    //this.scene.start('CentroScene');
+                    //this.scene.start('ShopScene');
+                    this.scene.start('CasaScene');
                     //this.scene.start('EscenaInicial');
 
                 }
